1fe3ca620398d4a05e094130e04644244eda5da8
ZaurNatok <61757081+ZaurNatok@users.noreply.github.com>
2023-04-11 12:04:46 +0300

    Add files via upload

---
+++ script-main.js
@@ -0,0 +1,50 @@
+let contactsBtn = document.querySelector('.contacts-link');
+let paymentsBtn = document.querySelector('.payments-link');
+let deliveryBtn = document.querySelector('.delivery-link');
+let refundBtn = document.querySelector('.refund-link');
+let closeBtn = document.querySelector('.close');
+let modalTitle = document.querySelector('.modal__title');
+let modalText = document.querySelector('.modal__text');
+
+let modal = document.querySelector('.modal');
+
+
+    contacts = {
+        title: 'Контактная информация',
+        text: 'Тут будут контакты'
+    },
+    payment = {
+        title: 'Способы оплаты',
+        text: 'Тут будут cпособы оплаты'
+    },
+    delivery = {
+        title: 'Условия доставки',
+        text: 'Тут будут условия доставки'
+    },
+    refund = {
+        title: 'Возврат товара',
+        text: 'Тут будут условия возврата'  
+    }
+
+
+document.addEventListener('click', (e) => {
+    if(e.target == contactsBtn) {
+        modal.classList.toggle('invisible');
+        modalTitle.textContent = contacts.title;
+        modalText.textContent = contacts.text;
+    } else if(e.target == paymentsBtn) {
+        modal.classList.toggle('invisible');
+        modalTitle.textContent = payment.title;
+        modalText.textContent = payment.text;
+    } else if(e.target == deliveryBtn) {
+        modal.classList.toggle('invisible');
+        modalTitle.textContent = delivery.title;
+        modalText.textContent = delivery.text;
+    } else if(e.target == refundBtn) {
+        modal.classList.toggle('invisible');
+        modalTitle.textContent = refund.title;
+        modalText.textContent = refund.text;
+    } else if(e.target == closeBtn || !e.target.classList.contains('modal-window')) {
+        modal.classList.toggle('invisible');
+    }
+})
---
+++ script.js
@@ -0,0 +1,31 @@
+var retrievedClient = localStorage.getItem('client');
+let finalClient = JSON.parse(retrievedClient);
+
+let video = document.querySelector('.ytp-cued-thumbnail-overlay-image');
+
+let url = 'https://api.sheety.co/fab97654fa94675e6ddbd722da66e294/gang/clients';
+let body = {
+        "client": {
+            "name": finalClient.name,
+            "phone": finalClient.phone,
+            "email": finalClient.email,
+            "address": finalClient.address,
+            "price":  finalClient.price,
+            "date": finalClient.date
+        }
+  }
+ 
+  fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type':'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+  .then((response) => response.json())
+  .then(json => {
+    // Do something with object
+    console.log(json.client);
+  });
+
+console.log(video)
